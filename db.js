626799fc8027852f16770124acf3fa2459168d69
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-01-06 11:44:35 +0100

    change structure data and add resolvers

--- db.js
+++ db.js
@@ -2,206 +2,129 @@ const data = {
   books: [
     {
       title: "Harry Potter and the Sorcerer's Stone",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/harry1.jpg"
-      }
+      coverPath: "/images/book-covers/harry1.jpg"
     },
     {
       title: "Harry Potter and the Chamber of Secrets",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/harry2.jpg"
-      }
+      coverPath: "/images/book-covers/harry2.jpg"
     },
     {
       title: "Harry Potter and the Prisoner of Azkaban",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/harry3.jpg"
-      }
+      coverPath: "/images/book-covers/harry3.jpg"
     },
     {
       title: "Harry Potter and the Goblet of Fire",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/harry4.jpg"
-      }
+      coverPath: "/images/book-covers/harry4.jpg"
     },
     {
       title: "Harry Potter and the Order of the Phoenix",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/harry5.jpg"
-      }
+      coverPath: "/images/book-covers/harry5.jpg"
     },
     {
       title: "Harry Potter and the Half-Blood Prince",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/harry6.jpg"
-      }
+      coverPath: "/images/book-covers/harry6.jpg"
     },
     {
       title: "Harry Potter and the Deathly Hallows",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/harry7.jpg"
-      }
+      coverPath: "/images/book-covers/harry7.jpg"
     },
     {
       title: "Leviathan Wakes",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/expanse1.jpg"
-      }
+      coverPath: "/images/book-covers/expanse1.jpg"
     },
     {
       title: "Caliban's War",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/expanse2.jpg"
-      }
+      coverPath: "/images/book-covers/expanse2.jpg"
     },
     {
       title: "Abaddon's Gate",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/expanse3.jpg"
-      }
+      coverPath: "/images/book-covers/expanse3.jpg"
     },
     {
       title: "Cibola Burn",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/expanse4.jpg"
-      }
+      coverPath: "/images/book-covers/expanse4.jpg"
     },
     {
       title: "Nemesis Games",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/expanse5.jpg"
-      }
+      coverPath: "/images/book-covers/expanse5.jpg"
     },
     {
       title: "Babylon's Ashes",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/expanse6.jpg"
-      }
+      coverPath: "/images/book-covers/expanse6.jpg"
     },
     {
       title: "Persepolis Rising",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/expanse7.jpg"
-      }
+      coverPath: "/images/book-covers/expanse7.jpg"
     },
     {
       title: "Tiamat's Wrath",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/expanse8.jpg"
-      }
+      coverPath: "/images/book-covers/expanse8.jpg"
     },
     {
       title: "Blood of Elves",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/witcher1.jpg"
-      }
+      coverPath: "/images/book-covers/witcher1.jpg"
     },
     {
       title: "Time of contempt",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/witcher2.jpg"
-      }
+      coverPath: "/images/book-covers/witcher2.jpg"
     },
     {
       title: "Baptism of fire",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/witcher3.jpg"
-      }
+      coverPath: "/images/book-covers/witcher3.jpg"
     },
     {
       title: "The tower of the swallow",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/witcher4.jpg"
-      }
+      coverPath: "/images/book-covers/witcher4.jpg"
     },
     {
       title: "The lady of the lake",
-      cover: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-covers/witcher5.jpg"
-      }
+      coverPath: "/images/book-covers/witcher5.jpg"
     }
   ],
   authors: [
     {
       name: "J. K. Rowling",
-      photo: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-authors/j-k-rowling.jpg"
-      }
+      photoPath: "/images/book-authors/j-k-rowling.jpg"
     },
     {
       name: "James S. A. Corey",
-      photo: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-authors/james-s-a-corey.jpg"
-      }
+      photoPath: "/images/book-authors/james-s-a-corey.jpg"
     },
     {
       name: "Andrzej Sapkowski",
-      photo: {
-        url:
-          "http://examples.devmastery.pl/assets/images/book-authors/andrzej-sapkowski.jpg"
-      }
+      photoPath: "/images/book-authors/andrzej-sapkowski.jpg"
     }
   ],
   users: [
     {
-      name: "Alice Keys",
+      name: "Alice",
       email: "alice@example.com",
       avatar: {
-        image: {
-          url: "http://examples.devmastery.pl/assets/images/avatars/w13.png"
-        },
+        imagePath: "/images/avatars/w13.png",
         color: "yellow"
       }
     },
     {
-      name: "Bob Dylan",
+      name: "Bob",
       email: "bob@example.com",
       avatar: {
-        image: {
-          url: "http://examples.devmastery.pl/assets/images/avatars/m10.png"
-        },
+        imagePath: "/images/avatars/m10.png",
         color: "green"
       }
     },
     {
-      name: "Celine Dion",
+      name: "Celine",
       email: "celine@example.com",
       avatar: {
-        image: {
-          url: "http://examples.devmastery.pl/assets/images/avatars/w2.png"
-        },
+        imagePath: "/images/avatars/w2.png",
         color: "red"
       }
     },
     {
-      name: "Dan Abramov",
-      email: "DAN@example.com",
+      name: "Dan",
+      email: "dan@example.com",
       avatar: {
-        image: {
-          url: "http://examples.devmastery.pl/assets/images/avatars/m25.png"
-        },
+        imagePath: "/images/avatars/m25.png",
         color: "blue"
       }
     }
--- resolvers.js
+++ resolvers.js
@@ -1,12 +1,27 @@
 const db = require("./db");
+const BASE_ASSETS_URL = "http://examples.devmastery.pl/assets";
 
 const resolvers = {
   Author: {
-    books: parent => parent.bookIds.map(db.getBookById)
+    books: parent => parent.bookIds.map(db.getBookById),
+    photo: parent => ({
+      path: parent.photoPath
+    })
+  },
+  Avatar: {
+    image: parent => ({
+      path: parent.imagePath
+    })
   },
   Book: {
     title: parent => parent.title.toUpperCase(),
-    author: parent => db.getAuthorById(parent.authorId)
+    author: parent => db.getAuthorById(parent.authorId),
+    cover: parent => ({
+      path: parent.coverPath
+    })
+  },
+  Image: {
+    url: parent => BASE_ASSETS_URL + parent.path
   },
   User: {
     email: parent => parent.email.toLowerCase(),
